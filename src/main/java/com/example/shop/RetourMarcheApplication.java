99ec2dc52fe10db6d685502880060eefe3d26080
RaphCzt <raphaelcazet@gmail.com>
2025-11-21 12:33:56 +0100

    Premier comm

---
+++ src/main/java/com/example/shop/RetourMarcheApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetourMarcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetourMarcheApplication.class, args);
+	}
+
+}
